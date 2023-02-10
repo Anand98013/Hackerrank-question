699654e963c4eeecb74a4586a6e8d80f1fb5aec9
Anand Kumar <111699706+Anand98013@users.noreply.github.com>
2023-02-10 17:22:41 +0530

    Add files via upload

---
+++ playing_with_character.c
@@ -0,0 +1,28 @@
+/*
+Sample Input 0
+C
+Language
+Welcome To C!!
+
+Sample Output 0
+C
+Language
+Welcome To C!!
+*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    char ch;
+    char s[24];
+    char sen[100];
+    scanf("%c\n%s\n%[^\n]s",&ch,s,sen);
+    printf("%c\n%s\n%s",ch,s,sen);
+    return 0;
+}
